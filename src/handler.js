a0470672e13e7abf41e7fadeff555a40ff09f13a
Marco Reinwarth <marco.reinwarth@leanovate.de>
2018-03-29 17:59:43 +0200

    Add basic write CQRS infrastructure
     * write dummy data to DynamoDB via REST call
     * trigger lambda function on DynamoDB changes with stream functionality

--- src/handler.js
+++ src/handler.js
@@ -14,3 +14,54 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.db = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 15; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      text: text,
+      checked: false,
+      timestamp: `${timestamp}`,
+      timestampSorted: timestamp,
+    },
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: `Couldn't put item ${text}`,
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
+
+module.exports.logdb = (event, context, callback) => {
+    console.log(JSON.stringify(event));
+};
