352d96dbc00df248ecc49be196ef735fa66afeb1
Marco Reinwarth <marco.reinwarth@leanovate.de>
2018-04-10 17:49:48 +0200

    Remove hello world function

--- src/lambda-handlers/handler.js
+++ src/lambda-handlers/handler.js
@@ -1,20 +1,5 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 module.exports.logdb = (event, context, callback) => {
     console.log(JSON.stringify(event));
     console.log("id: " + event.Records[0].dynamodb.NewImage.id.S);
