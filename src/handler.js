fe1b749486e78d3a1240778772fd8f69683a4c78
Philipp Rosenkranz <philipp.rosenkranz@leanovate.de>
2018-04-10 14:21:21 +0200

    factored out projection handler + fixes to serverless.yml

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,5 @@
 'use strict';
 
-import RedisCache from '../helper/RedisCache'
-
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -17,81 +15,7 @@ module.exports.hello = (event, context, callback) => {
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
 
-const AWS = require('aws-sdk');
-
 module.exports.logdb = (event, context, callback) => {
     console.log(JSON.stringify(event));
     console.log("id: " + event.Records[0].dynamodb.NewImage.id.S);
 };
-
-module.exports.projection = async (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false
-
-    const CACHE_KEY = 'CACHE_KEY'
-    let res = {}
-    let checkCache = await RedisCache.get(CACHE_KEY)
-    if (checkCache) {
-        res = checkCache
-    } else {
-        await RedisCache.set(CACHE_KEY, {'message': 'Hello World!'})
-        res = {'message': 'Set cache success!'}
-    }
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(res)
-    }
-    callback(null, response);
-/*
-  let client;
-  try {
-    client = require('redis').createClient(6379, 'readcache.cg0t9q.0001.euc1.cache.amazonaws.com', {no_ready_check: true});
-    console.log("Connection to Redis with client was successfull");
-
-      
-    console.log(`Going to save event ${JSON.stringify(event)}`);
-    const body = event.Records[0].dynamodb.NewImage;
-    const key = body.id.S;
-
-    client.set(key, JSON.stringify(body), (error) => {
-        // handle potential errors
-        if (error) {
-            console.error(error);
-            return;
-        }
-
-        console.log(`Projected into view cache key: ${key} body: ${JSON.stringify(body)}`);
-
-        
-        client.get(key, (error, data) => {
-            // handle potential errors
-            if (error) {
-                console.error(error);
-                return;
-            }
-            
-            console.log(`Fetched for key: ${key} data: ${JSON.stringify(data)}`);
-            
-            client.quit();
-
-            const response = {
-              statusCode: 200,
-              body: "Connection to Redis with client was successfull",
-            };
-            callback(null, response);
-        });
-
-    });
-      
-  } catch(e) {
-    console.error("Cannot connect to Redis with client");
-    console.error(e);
-      
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify(e),
-    };
-    callback(null, response);
-
-  }
-*/
-}
